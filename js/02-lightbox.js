0262eb28d2def0242d545ad7fe84bccb5fb48022
Vitalii Shtanko <patas323f@gmail.com>
2022-08-18 22:39:15 +0300

    Второе задание закончил

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from './gallery-items.js'
 // Change code below this line
 
-console.log(galleryItems);
+console.log(galleryItems)
+
+const listGallery = document.querySelector('.gallery')
+
+// Создаем переменную для добавления разметки
+
+const markupGalleryEl = markupGalleryList(galleryItems)
+//console.log(markupGalleryEl)
+
+function markupGalleryList() {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class = gallery_lists>
+         <a class = gallery__item href='${original}'>
+      <img
+      class= 'gallery__image'
+      src= '${preview}'
+      
+      alt='${description}'
+    />
+  </a>
+</li>`
+    })
+    .join('')
+}
+
+// Вставляем разметку в html
+listGallery.insertAdjacentHTML('afterbegin', markupGalleryEl)
+console.log(listGallery)
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+})
