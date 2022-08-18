88d0b61f64e89ca6a3cb125559c224f3b223de9c
Vitalii Shtanko <patas323f@gmail.com>
2022-08-18 09:48:10 +0300

    Создал разметку html

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,36 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from './gallery-items.js'
 // Change code below this line
 
-console.log(galleryItems);
+console.log(galleryItems)
+
+const containerGallery = document.querySelector('.gallery')
+
+const image = document.querySelector('.gallery__image')
+
+// Вешаем слушателя на Div
+
+//containerGallery.addEventListener('click' onClickModal)
+
+// Создаем переменную для добавления разметки
+
+const markupGalleryEl = markupDataArr(galleryItems)
+console.log(markupGalleryEl)
+
+function markupDataArr() {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class = gallery_item>
+         <a class = gallery__link href='${original}'>
+      <img
+      class= 'gallery__image'
+      src= '${preview}'
+      data-source= '${original}'
+      alt='${description}'
+    />
+  </a>
+</div>`
+    })
+    .join('')
+}
+
+//function onClickModal ()
